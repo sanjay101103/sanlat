2f0ae8779167a0cd46ef3ce7e5508027f6ef10f9
kgm <145899968+sanjay101103@users.noreply.github.com>
2023-10-07 11:55:06 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+function createHeart() {
+
+  heart.classList.add('heart');
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+  
+  heart.innerText = '';
+  
+  document.body.appendChild(heart);
+  
+  setTimeout(() =>{
+              heart.remove();
+               5000);
+}
+			 }
+setInterval(createHeart, 300);
